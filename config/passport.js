012550fa4b2ac10a1be3f4cb94f455b64ca8072b
Petra-78 <partos.petra.0305@gmail.com>
2025-12-11 16:59:04 +0100

    Add passport config with validation and pwd hashing

---
+++ config/passport.js
@@ -0,0 +1,47 @@
+import passport from "passport";
+import bcrypt from "bcryptjs";
+import { Strategy as LocalStrategy } from "passport-local";
+import { prisma } from "../lib/prisma.js";
+
+passport.use(
+  new LocalStrategy(async (username, password, done) => {
+    try {
+      const user = await prisma.users.findUnique({
+        where: { username },
+      });
+
+      if (!user) {
+        return done(null, false, { message: "Incorrect username" });
+      }
+
+      const match = await bcrypt.compare(password, user.password);
+      if (!match) {
+        return done(null, false, { message: "Incorrect password" });
+      }
+
+      return done(null, user);
+    } catch (err) {
+      return done(err);
+    }
+  })
+);
+
+passport.serializeUser((user, done) => {
+  done(null, user.id);
+});
+
+passport.deserializeUser(async (id, done) => {
+  try {
+    const user = await prisma.users.findUnique({
+      where: { id },
+    });
+
+    if (!user) {
+      return done(null, false);
+    }
+
+    done(null, user);
+  } catch (err) {
+    done(err);
+  }
+});
