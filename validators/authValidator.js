a81954857a8577efdc6965bed5b7de1a6381dc79
Petra-78 <partos.petra.0305@gmail.com>
2025-12-11 16:54:54 +0100

    Add signup validation

---
+++ validators/authValidator.js
@@ -0,0 +1,57 @@
+const { body } = require("express-validator");
+import { prisma } from './lib/prisma.js'
+
+const signupValidator = [
+  body("email")
+    .trim()
+    .isEmail()
+    .withMessage("Email must be valid")
+    .custom(async (value) => {
+      const user = await prisma.users.findUnique({
+        where: { email: value },
+        select: { id: true }
+    });
+
+      if (user.length > 0) {
+        throw new Error("Account with this email already exists");
+      }
+
+      return true;
+    }),
+
+body("username")
+    .trim()
+    .isLength({ max: 20 })
+    .withMessage("Username must be max 20 characters long")
+    .custom(async (value) => {
+      const user = await prisma.users.findUnique({
+        where: { username: value },
+        select: { id: true }
+    });
+
+      if (user.length > 0) {
+        throw new Error("Account with this username already exists");
+      }
+
+      return true;
+    }),
+
+
+  body("password")
+    .trim()
+    .isLength({ min: 5, max: 16 })
+    .withMessage("Password must be 5–16 characters long")
+    .matches(/[A-Z]/)
+    .withMessage("Password must contain at least one uppercase letter")
+    .matches(/[^A-Za-z0-9]/)
+    .withMessage("Password must contain at least one special character"),
+
+  body("confirmPassword").custom((value, { req }) => {
+    if (value !== req.body.password) {
+      throw new Error("Passwords do not match");
+    }
+    return true;
+  }),
+];
+
+module.exports = { signupValidator };
