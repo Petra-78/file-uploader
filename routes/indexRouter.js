6b812009b10881196d52401e5e088221a3579119
Petra-78 <partos.petra.0305@gmail.com>
2025-12-12 17:59:45 +0100

    Add file posting logic with multer

--- routes/indexRouter.js
+++ routes/indexRouter.js
@@ -1,7 +1,13 @@
 import { Router } from "express";
+import path from "node:path";
 const router = Router();
 import { signUp, isAuthenticated } from "../controllers/authController.js";
 import { signupValidator } from "../validators/authValidator.js";
+import { postNewFile } from "../controllers/indexController.js";
+import multer from "multer";
+const upload = multer({
+  dest: path.join(process.cwd(), "public/uploads"),
+});
 
 router.get("/", (req, res) => {
   res.render("index");
@@ -11,5 +17,6 @@ router.post("/sign-up", signupValidator, signUp);
 router.get("/new-file", (req, res) => {
   res.render("new-file");
 });
+router.post("/new-file", isAuthenticated, upload.single("file"), postNewFile);
 
 export default router;
