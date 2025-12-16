0b10ea88fff6acb9b7d5c287555826122d030e6f
Petra-78 <partos.petra.0305@gmail.com>
2025-12-16 19:50:40 +0100

    Add file details + file delete controllers

--- controllers/fileController.js
+++ controllers/fileController.js
@@ -1,4 +1,6 @@
 import { prisma } from "../lib/prisma.js";
+import path from "node:path";
+import fs from "fs";
 
 export async function renderNewFileForm(req, res, next) {
   try {
@@ -36,6 +38,48 @@ export async function postFile(req, res, next) {
     });
 
     console.log(files);
+
+    res.redirect(`/folders/${folderId}`);
+  } catch (err) {
+    next(err);
+  }
+}
+
+export async function fileDetails(req, res, next) {
+  try {
+    const folderId = Number(req.params.folderId);
+    const fileId = Number(req.params.fileId);
+
+    const file = await prisma.files.findUnique({
+      where: { id: fileId },
+      include: { folder: true },
+    });
+
+    if (!file || file.userId !== req.user.id) {
+      return res.status(403).send("Unauthorized");
+    }
+
+    res.render("partials/file-details", { file, folderId });
+  } catch (err) {
+    next(err);
+  }
+}
+
+export async function deleteFile(req, res, next) {
+  try {
+    const fileId = Number(req.params.fileId);
+    const folderId = Number(req.params.folderId);
+
+    const file = await prisma.files.findUnique({ where: { id: fileId } });
+
+    if (!file || file.userId !== req.user.id) {
+      return res.status(403).send("Unauthorized");
+    }
+
+    const filePath = path.join(process.cwd(), "public", file.fileUrl);
+    if (fs.existsSync(filePath)) fs.unlinkSync(filePath);
+
+    await prisma.files.delete({ where: { id: fileId } });
 
     res.redirect(`/folders/${folderId}`);
   } catch (err) {
