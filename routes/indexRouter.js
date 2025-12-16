ed85849b860a117c6c1e058de2ff298c068ce013
Petra-78 <partos.petra.0305@gmail.com>
2025-12-16 19:34:09 +0100

    Update url links to new file route

--- routes/indexRouter.js
+++ routes/indexRouter.js
@@ -3,7 +3,7 @@ import path from "node:path";
 const router = Router();
 import { signUp, isAuthenticated } from "../controllers/authController.js";
 import { signupValidator } from "../validators/authValidator.js";
-import { postNewFile } from "../controllers/indexController.js";
+import { postFile, renderNewFileForm } from "../controllers/fileController.js";
 import multer from "multer";
 import { renderDashboard } from "../controllers/folderController.js";
 const upload = multer({
@@ -13,9 +13,13 @@ const upload = multer({
 router.get("/", renderDashboard);
 
 router.post("/sign-up", signupValidator, signUp);
-router.get("/new-file", (req, res) => {
-  res.render("new-file");
-});
-router.post("/new-file", isAuthenticated, upload.single("file"), postNewFile);
+
+router.get("/folders/:folderId/new-file", isAuthenticated, renderNewFileForm);
+router.post(
+  "/folders/:folderId/new-file",
+  isAuthenticated,
+  upload.single("file"),
+  postFile
+);
 
 export default router;
