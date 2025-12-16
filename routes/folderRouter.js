649b5303e1264eeb4f69b4f9e1bbb2ac5888287e
Petra-78 <partos.petra.0305@gmail.com>
2025-12-16 18:40:16 +0100

    Update folders route, import controller + add dashboard rendering

--- routes/folderRouter.js
+++ routes/folderRouter.js
@@ -1,16 +1,23 @@
 import { Router } from "express";
-import multer from "multer";
 
-import { createFolder, deleteFolder } from "../controllers/folderController.js";
+import {
+  createFolder,
+  deleteFolder,
+  renderDashboard,
+} from "../controllers/folderController.js";
 import { isAuthenticated } from "../controllers/authController.js";
 
 const router = Router();
 
 router.get("/new-folder", isAuthenticated, (req, res) => {
-  res.render("new-folder");
+  res.render("partials/new-folder", {
+    parentId: req.query.parentId || null,
+  });
 });
+
 router.post("/new-folder", isAuthenticated, createFolder);
 
+router.get("/folders/:id", isAuthenticated, renderDashboard);
 router.post("/folders/:id/delete", isAuthenticated, deleteFolder);
 
 export default router;
