188014aed8a0253be8a7107ef491dd3c24ccfb26
Petra-78 <partos.petra.0305@gmail.com>
2025-12-17 20:07:31 +0100

    Update config to store in memory

--- config/multer.js
+++ config/multer.js
@@ -1,14 +1,4 @@
 import multer from "multer";
-import path from "node:path";
 
-const storage = multer.diskStorage({
-  destination: function (req, file, cb) {
-    cb(null, path.join("public", "uploads"));
-  },
-  filename: function (req, file, cb) {
-    const uniqueSuffix = Date.now() + "-" + Math.round(Math.random() * 1e9);
-    cb(null, uniqueSuffix + path.extname(file.originalname));
-  },
-});
-
+const storage = multer.memoryStorage();
 export const upload = multer({ storage });
